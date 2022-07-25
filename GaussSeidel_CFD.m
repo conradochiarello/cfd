554924a9ec34324c2ae0db8ef760ad4c5faf4815
conradochiarello <55289667+conradochiarello@users.noreply.github.com>
2022-07-25 10:18:32 -0600

    Update GaussSeidel_CFD.m

---
+++ GaussSeidel_CFD.m
@@ -0,0 +1,70 @@
+function GaussSeidel_CFD(N)
+
+% Gauss-Seidel Algorithm for Diffusion 1D Steady-State without
+% internal energy generation and given edge temperatures
+% Author: Conrado Chiarello
+% NUEM - Multiphase Flow Research Center
+% Professor: Dr. Paulo H. D. Santos
+tic
+% Given temperatures
+Tp1 = 150;
+Tp2 = 50;
+
+% Initializing variables for code performance
+a_p = zeros(N,1);
+a_e = zeros(N,1);
+a_w = zeros(N,1);
+S_u = zeros(N,1);
+S_p = zeros(N,1);
+res = zeros(N,1);
+
+% Initial guess
+T = ones(N,1)*(0.5*(Tp1 + Tp2));
+
+while (1)
+    for i = 1 : N
+        if i == 1
+            a_e(i) = 1;
+            a_w(i) = 0;
+            S_u(i) = 2*Tp1;
+            S_p(i) = -2;
+            a_p(i) = a_e(i) + a_w(i) - S_p(i);
+            
+            T(i) = (a_e(i)*T(i+1) + S_u(i))/a_p(i);
+            
+        elseif i < N
+            a_e(i) = 1;
+            a_w(i) = 1;
+            S_u(i) = 0;
+            S_p(i) = 0;
+            a_p(i) = a_e(i) + a_w(i) - S_p(i);
+            
+            T(i) = (a_e(i).*T(i+1) + a_w(i).*T(i-1) + S_u(i))./a_p(i);
+            
+        elseif i == N
+            a_e(i) = 0;
+            a_w(i) = 1;
+            S_u(i) = 2*Tp2;
+            S_p(i) = -2;
+            a_p(i) = a_e(i) + a_w(i) - S_p(i);
+            
+            T(i) = (a_w(i)*T(i-1) + S_u(i))/a_p(i);
+        end
+    end
+    
+    for i = 1 : N
+        if i == 1
+            res(i) = S_u(i) + a_e(i)*T(i+1) - a_p(i)*T(i);
+        elseif i < N
+            res(i) = S_u(i) + a_e(i)*T(i+1) + a_w(i)*T(i-1) - a_p(i)*T(i);
+        elseif i == N
+            res(i) = S_u(i) + a_w(i)*T(i-1) - a_p(i)*T(i);
+        end
+    end
+    RMS = sqrt(sum(res.^2));
+    
+    if RMS < 1E-3
+        break
+    end
+end
+toc
